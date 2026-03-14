f762ccbd7a0214a4da7cb79381cf4f5b1a77aa5a
Bogdan Sandu <bogdanelsandu2011@gmail.com>
2026-03-14 11:52:45 +0200

    Update userids

--- src/content/core/configs/userIds.js
+++ src/content/core/configs/userIds.js
@@ -10,7 +10,8 @@ export const CONTRIBUTOR_USER_IDS = [
     '546872490', //Kanibal02
     '48255812', //aliceenight
     '7982684834', //qborder
-    '126448532' //steinann
+    '126448532', //steinann
+    '1564574922', // cornusandu
 ];
 
 export const RAT_BADGE_USER_ID = '477516666'; // rat
