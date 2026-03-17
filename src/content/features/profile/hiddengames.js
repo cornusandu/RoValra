06984c17a30611d48b0ac2610c79e4ffee14458b
NotValra <valra@rovalra.com>
2026-03-17 14:17:26 +0100

    I assume u removed this by accident

--- src/content/features/profile/hiddengames.js
+++ src/content/features/profile/hiddengames.js
@@ -7,7 +7,7 @@ import { fetchThumbnails as fetchThumbnailsBatch } from '../../core/thumbnail/th
 import { callRobloxApi } from '../../core/api.js';
 import { safeHtml } from '../../core/packages/dompurify';
 import { createGameCard } from '../../core/ui/games/gameCard.js';
-
+import { t } from 'i18next';
 const CONFIG = {
     PAGE_SIZE: 50,
     ACCESS_FILTER: 2,
